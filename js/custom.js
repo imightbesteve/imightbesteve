d2c7a2d84fe10fa092ba39437537c00f55720122
imightbesteve <stevebuffee@gmail.com>
2016-11-22 17:04:42 -0800

    Changed to Final Project Files

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,12 @@
-$( document ).ready(function() {
-  //console.log( "ready!" );
+$(document).ready(function() {
+	$('.opnbtn').click(function() { 
+		console.log('button is clicked');
+	    $(".before").delay(380).fadeOut();
+	    $(".entry-button").delay(380).fadeOut();
+	    $(".after").delay(800).fadeIn();
+	  });
+	$('.carousel').carousel({
+	   interval: 0
+	})
 });
+
